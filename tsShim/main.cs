6f818a03d0c36eb3b387dab35c2a46a0a43941a1
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-07-31 23:02:02 +1000

    It was bugging me.

--- tsShim/main.cs
+++ tsShim/main.cs
@@ -2,6 +2,10 @@ function SimSet::contains(%this, %obj) {
    return %this.getObjectIndex(%obj) != -1;
 }
 
+function SimSet::size(%this) {
+   return %this.getCount();
+}
+
 function include(%path) {
    exec(%path @ "/main.cs");
 }

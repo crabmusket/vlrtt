23b14d4f47a2d1325e11694f1bfcd29eed63d658
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-08-01 17:27:43 +1000

    Added enemy targeting.

--- game/enemies.cs
+++ game/enemies.cs
@@ -18,7 +18,25 @@ function soldier(%pos) {
 
 function Enemies::onStart(%this) {
    GameGroup.add(Enemies);
+
+   // Create a keymap that accepts every key.
+   %this.targetMap = new ActionMap();
+   %this.letters = "jkhnfdgvcbtyursieowpxqlazm";
+   for(%i = 0; %i < strLen(%this.letters); %i++) {
+      %action = getSubStr(%this.letters, %i, 1);
+      echo(%action);
+      %this.targetMap.bindCmd(keyboard, %action, "Enemies.target(" @ %action @ ");");
+   }
 }
 
 function Enemies::onEnd(%this) {
+   %this.targetMap.delete();
+}
+
+function Enemies::target(%this, %char) {
+   %index = strPos(%this.letters, %char);
+   if(%index < %this.size()) {
+      Verbs.target = %this.getObject(%index);
+      Verbs.onEvent(enemyTargeted);
+   }
 }

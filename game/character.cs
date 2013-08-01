ba341838bf328f8484939c6138f72a526adb42de
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-08-01 21:09:48 +1000

    Whoops, add character class.

---
+++ game/character.cs
@@ -0,0 +1,22 @@
+function SceneObject::damage(%this, %amount) {
+   if(%this.can(getDataBlock)) {
+      if(%this.getDataBlock().can(damage)) {
+         %this.getDataBlock().damage(%this, %amount);
+      }
+   }
+}
+
+function Character::damage(%this, %obj, %amount) {
+   %obj.applyDamage(%amount);
+   if(%obj.getDamagePercent() > 1) {
+      %this.onDestroyed(%obj);
+   } else {
+      %this.onDamaged(%obj, %amount);
+   }
+}
+
+function Character::onDestroyed(%this, %obj) {
+   %obj.delete();
+}
+
+function Character::onDamaged(%this, %obj, %amount) {}

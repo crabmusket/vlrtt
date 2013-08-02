09815771572643724371942dc6b635879c705b27
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-08-02 22:21:31 +1000

    Added an easy block creation function.

--- game/level.cs
+++ game/level.cs
@@ -1,5 +1,21 @@
 new ScriptObject(Level1);
 
+function block(%pos, %size) {
+   TheLevel.add(new ConvexShape() {
+      material = BlankWhite;
+      position = %pos;
+      rotation = "1 0 0 0";
+      scale = %size;
+
+      surface = "0 0 0 1 0 0 0.5";
+      surface = "0 1 0 0 0 0 -0.5";
+      surface = "0.707107 0 0 0.707107 0 0.5 0";
+      surface = "0 0.707107 -0.707107 0 0 -0.5 0";
+      surface = "0.5 0.5 -0.5 0.5 -0.5 0 0";
+      surface = "0.5 -0.5 0.5 0.5 0.5 0 0";
+   });
+}
+
 function Level1::onStart() {
    GameGroup.add(new SimGroup(TheLevel) {
       new LevelInfo(TheLevelInfo) {

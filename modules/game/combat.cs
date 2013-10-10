b5a1cb1ec4bf4903218ac8bd80a0f1d8714b1222
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-10-10 14:36:55 +1100

    Worked om combat scripts

--- modules/game/combat.cs
+++ modules/game/combat.cs
@@ -1,6 +1,6 @@
 new ScriptObject(Combats);
 
-$UpdateENergyPeriod = 500; // Milliseconds.
+$UpdateEnergyPeriod = 500; // Milliseconds.
 
 function Character::onCombatBegin(%this, %obj, %data) {
    %subject = getWord(%data, 0);
@@ -36,74 +36,33 @@ function AIPlayer::updateEnergy(%obj) {
    %obj.schedule($UpdateEnergyPeriod, updateEnergy);
 
    if(%obj.getEnergyLevel() <= 1) {
-      postEvent(Combat, 
-}
-
-function AIPlayer::joinCombat(%obj, %combat) {
-   %obj.getDataBlock().joinCombat(%obj, %combat);
-}
-
-function Character::joinCombat(%this, %obj, %enemy) {
-   // Focus on combat!
-   %obj.stopAll();
-   %obj.follow(%enemy);
-}
-
-function Combats::begin(%this, %attacker, %defender, %mutual) {
-   %c = new ScriptObject() {
-      class = Combat;
-      attacker = %attacker;
-      defender = %defender;
-   };
-   %attacker.combat = %c;
-   %defender.combat = %c;
-   %attacker.joinCombat(%defender, %mutual);
-   %defender.joinCombat(%attacker, %mutual);
-   %c.fight();
-}
-
-function Combat::blows(%this, %attacker, %defender) {
-   //echo("defender energy:" SPC %defender.getEnergyLevel());
-   if(%defender.getEnergyLevel() < 1) {
-      %amount = getRandom(40, 120);
-      %defender.damage(%amount);
-      //echo(%attacker SPC "hits" SPC %defender SPC "for" SPC %amount);
-      %defender.schedule(100, adrenaline, 20);
-      %attacker.schedule(100, adrenaline, 10);
+      postEvent(Combat, antExhausted /* lol */, %obj);
    }
 }
 
-function Combat::aftermath(%this, %obj) {
-   if(%obj.getDamagePercent() >= 1) {
-      %this.shouldEnd = true;
+function Character::onCombatantExhausted(%this, %obj, %enemy) {
+   if(%enemy.isAWordIn(%obj.fighting)) {
+      // Strike a blow!
+      %enemy.damage(getRandom(40, 120));
+      %subject.adrenaline(10);
+      %enemy.adrenaline(20);
    }
 }
 
-function Combat::prepare(%this, %attacker, %defender) {
+function Character::onCharacterDeath(%this, %obj, %character) {
+   if(%obj.fighting) {
+      %obj.removeEnemy(%character);
+   }
 }
 
-function Combat::release(%this, %obj) {
-   %obj.stopAll();
-   %obj.combat = "";
-   %obj.setRechargeRate(0.1);
-}
-
-function Combat::fight(%this) {
-   // Calculate SPS
-   %this.prepare(%this.attacker, %this.defender);
-   %this.prepare(%this.defender, %this.attacker);
-   // Perform blows.
-   %this.blows(%this.attacker, %this.defender);
-   %this.blows(%this.defender, %this.attacker);
-   // Clean up.
-   %this.aftermath(%this.attacker);
-   %this.aftermath(%this.defender);
-   if(%this.shouldEnd) {
-      %this.release(%this.defender);
-      %this.release(%this.attacker);
-      %this.schedule(100, delete);
-   } else {
-      %this.fighting = %this.schedule(500, fight);
+function AIPlayer::removeEnemy(%obj, %enemy) {
+   %i = 0;
+   foreach$(%c in %obj.fighting) {
+      if(%c == %character) {
+         %obj.fighting = removeWord(%obj.fighting, %i);
+         break;
+      }
+      %i++;
    }
 }
 

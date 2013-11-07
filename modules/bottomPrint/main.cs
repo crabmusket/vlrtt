b60a8598d9d7ec6a64b3cc5412ae4c76c5543ff2
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-11-08 10:54:13 +1100

    Fixed bottom print and verb help text colour.

--- modules/bottomPrint/main.cs
+++ modules/bottomPrint/main.cs
@@ -1,5 +1,6 @@
 new ScriptObject(BottomPrint);
 
+exec("./profiles.cs");
 exec("./bottomPrint.gui");
 
 function BottomPrint::onStart(%this) {
---
+++ modules/bottomPrint/profiles.cs
@@ -0,0 +1,5 @@
+new GuiControlProfile(CenterPrintTextProfile) {
+   fontType = ($platform $= "macos") ? "Monaco" : "Lucida Console";
+   fontSize = ($platform $= "macos") ? 13 : 12;
+   fontColor = "255 255 255";
+};
